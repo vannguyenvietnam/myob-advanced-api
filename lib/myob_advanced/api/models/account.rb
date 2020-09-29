6539d65b6cbb3d2491c0f5370711024e2e7e67be
Van Nguyen <bichvannguyenvnn@gmail.com>
2020-09-29 13:07:14 +0700

    Add account model.

---
+++ lib/myob_advanced/api/models/account.rb
@@ -0,0 +1,11 @@
+module MyobAdvanced
+    module Api
+      module Model
+        class Account < Base
+          def model_route
+            'Account'
+          end
+        end
+      end
+    end
+  end
