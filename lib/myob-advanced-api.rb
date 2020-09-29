38a42a466316f2265a8deeef09856dec861f8529
Van Nguyen <bichvannguyenvnn@gmail.com>
2020-09-29 13:28:43 +0700

    Fix bugs.

--- lib/myob-advanced-api.rb
+++ lib/myob-advanced-api.rb
@@ -1,11 +1,10 @@
 require 'myob_advanced/api/version'
-
 require 'myob_advanced/api/helpers'
 
 require 'myob_advanced/api/models/base'
 
+require 'myob_advanced/api/models/account'
+require 'myob_advanced/api/models/customer'
 require 'myob_advanced/api/models/invoice'
-
-require 'myob_advanced/api/models/general_ledger'
 
 require 'myob_advanced/api/client'
