186db12b809a66464dc0366922e557fa20218865
Van Nguyen <bichvannguyenvnn@gmail.com>
2020-10-06 20:00:53 +0700

    Add models.

--- lib/myob-advanced-api.rb
+++ lib/myob-advanced-api.rb
@@ -4,7 +4,12 @@ require 'myob_advanced/api/helpers'
 require 'myob_advanced/api/models/base'
 
 require 'myob_advanced/api/models/account'
+
 require 'myob_advanced/api/models/customer'
+require 'myob_advanced/api/models/contact'
+require 'myob_advanced/api/models/customer_class'
+require 'myob_advanced/api/models/customer_payment_method'
+
 require 'myob_advanced/api/models/invoice'
 require 'myob_advanced/api/models/sales_invoice'
 
---
+++ lib/myob_advanced/api/models/customer_payment_method.rb
@@ -0,0 +1,11 @@
+module MyobAdvanced
+    module Api
+      module Model
+        class CustomerPaymentMethod < Base
+          def model_route
+            'CustomerPaymentMethod'
+          end
+        end
+      end
+    end
+  end
