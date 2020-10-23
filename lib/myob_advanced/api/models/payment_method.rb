a47743e97ba9ccc473122e2dc5ca0200612b3ce9
Van Nguyen <bichvannguyenvnn@gmail.com>
2020-10-23 16:59:47 +0700

    Add payment method model.

---
+++ lib/myob_advanced/api/models/payment_method.rb
@@ -0,0 +1,11 @@
+module MyobAdvanced
+    module Api
+      module Model
+        class PaymentMethod < Base
+          def model_route
+            'PaymentMethod'
+          end
+        end
+      end
+    end
+  end
