5ce080877b07eba0e4b69b431c21c73f37ba2a16
Van Nguyen <bichvannguyenvnn@gmail.com>
2020-10-25 13:49:22 +0700

    Add vendor model.

---
+++ lib/myob_advanced/api/models/vendor_class.rb
@@ -0,0 +1,11 @@
+module MyobAdvanced
+    module Api
+      module Model
+        class VendorClass < Base
+          def model_route
+            'VendorClass'
+          end
+        end
+      end
+    end
+  end
