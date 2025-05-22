93f69a923a258df3a40f478ff73c92253444ac89
Nhan Nguyen <nhan@gos.com.vn>
2025-05-23 00:13:40 +0700

    Support OData for Credit Term.

---
+++ lib/myob_advanced/api/models/px_objects_cs_terms.rb
@@ -0,0 +1,23 @@
+module MyobAdvanced
+  module Api
+    module Model
+      class PxObjectsCsTerms < Base
+        def model_route
+          'PX_Objects_CS_Terms'
+        end
+
+        def self.field_id
+          'TermsID'
+        end
+
+        def self.field_name
+          'Descr'
+        end
+
+        def self.dac?
+          true
+        end
+      end
+    end
+  end
+end
